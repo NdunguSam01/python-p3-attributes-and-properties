0182cd5981196ee1dc6c4a68a8ef827d08bafcbc
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-12-12 12:03:50 +0300

    Desired result being produced but tests still failing

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        self.name=name
+        self.breed=breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self._breed="N/A"
+        else:
+            self._breed=breed
+    
+    name=property(get_name, set_name)
+    breed=property(get_breed, set_breed)
+
+Figo=Dog("Figo","Test")
+print(Figo.name)
+print(Figo.breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job):
+        self.name=name.title()
+        self.job=job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and len(name) <= 25:
+            self._name=name
+        else:
+            print("Name must be string under 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job=job
+        else:
+            print("Job must be in list of approved jobs.")
+            self._job="N/A"
+
+    name=property(get_name, set_name)
+    job=property(get_job, set_job)
+            
+Samuel=Person("muigai","test")
+print(Samuel.name)
+print(Samuel.job)
